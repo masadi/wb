6dfa07aa0b19af0316249b7385da3db3bc92e511
masadi <masadi.com@gmail.com>
2020-08-21 15:39:33 +0700

    Update sidebar.blade.php

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -121,7 +121,7 @@ $user = auth()->user();
                 <li class="nav-item">
                     <router-link tag="a" to="/kuisioner/pengisian" class="nav-link">
                         <i class="nav-icon fas fa-hand-point-right"></i>
-                        <p>Isi Kuisioner</p>
+                        <p>Kuisioner</p>
                     </router-link>
                 </li>
                 @endif
