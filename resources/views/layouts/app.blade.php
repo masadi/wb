49294fab8edb72151c965205a04ece0d8aaa51e7
masadi <masadi.com@gmail.com>
2020-08-26 11:02:45 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
                 <ul class="navbar-nav ml-auto order-3 order-md-3 navbar-no-expand">
                     @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Masuk Aplikasi') }}</a>
                     </li>
                     @if (Route::has('register'))
                     <li class="nav-item">
