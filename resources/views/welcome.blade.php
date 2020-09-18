d500ba51a0c8877fefe51a636e9e4275fc735374
masadi <masadi.com@gmail.com>
2020-09-18 09:23:45 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div id="apmIndikator" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#apmIndikator" data-slide-to="0" class="active"></li>
+                        <li data-target="#apmIndikator" data-slide-to="1"></li>
+                        <li data-target="#apmIndikator" data-slide-to="2"></li>
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -37,17 +37,12 @@
                                 src="{{asset('vendor/img/apm-5.jpg')}}"
                                 alt="Domain Aplikasi Penjaminan Mutu">
                         </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100"
-                                src="{{asset('vendor/img/apm-6.jpg')}}"
-                                alt="Launching Aplikasi Penjaminan Mutu">
-                        </div>
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <a class="carousel-control-prev" href="#apmIndikator" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="sr-only">Previous</span>
                     </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <a class="carousel-control-next" href="#apmIndikator" role="button" data-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                     </a>
