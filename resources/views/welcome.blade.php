45b45d22a6e353558309b5634aacbf74632e811e
masadi <masadi.com@gmail.com>
2020-09-30 23:00:27 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title">INFORMASI</h5>
+            <h5 class="modal-title">INFORMASI PENTING</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
