ed043440dc46f60f01d280496132c6bbbe8d9bef
masadi <masadi.com@gmail.com>
2020-09-30 23:11:30 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,11 +32,11 @@
                                 src="{{asset('vendor/img/apm-4.png')}}"
                                 alt="Alur Aplikasi Akses Direktorat">
                         </div>
-                        <div class="carousel-item">
+                        <!--div class="carousel-item">
                             <img class="d-block w-100"
                                 src="{{asset('vendor/img/apm-5.jpg')}}"
                                 alt="Domain Aplikasi Penjaminan Mutu">
-                        </div>
+                        </div-->
                     </div>
                     <a class="carousel-control-prev" href="#apmIndikator" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
