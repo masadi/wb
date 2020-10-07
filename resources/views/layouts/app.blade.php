31b1320263e405d91ba1f645b057b7e1c3e52620
masadi <masadi.com@gmail.com>
2020-10-08 00:17:01 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,8 +86,8 @@
                 <div class="collapse navbar-collapse order-2 ml-2" id="navbarCollapse">
                     <ul class="nav navbar-nav">
                         <li class="nav-item"><a class="nav-link" href="{{route('page', ['query' => 'berita'])}}">Berita</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{route('page', ['query' => 'progres-data'])}}">Progres Data</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{route('page', ['query' => 'rapor-mutu'])}}">Rapor Mutu</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{route('page', ['query' => 'progres-data'])}}" target="_blank">Progres Data</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{route('page', ['query' => 'rapor-mutu'])}}" target="_blank">Rapor Mutu</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{route('page', ['query' => 'galeri'])}}">Galeri</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{route('page', ['query' => 'faq'])}}">FAQ</a></li>
                     </ul>
