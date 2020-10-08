9bdf654ed111108044a8a72abf6bd8e3c342e948
masadi <masadi.com@gmail.com>
2020-10-08 09:50:41 +0700

    Update app_rapor.blade.php

--- resources/views/layouts/app_rapor.blade.php
+++ resources/views/layouts/app_rapor.blade.php
@@ -16,6 +16,11 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     <link rel='stylesheet' type="text/css" href='https://fonts.googleapis.com/css?family=Titillium+Web:400,600,700,300'>
     <!--link rel="shortcut icon" href="{{asset('favicons/favicon.ico')}}" /-->
+    <style>
+      .bg-custom {
+            background: #4abcb2 !important;
+        }
+    </style>
 </head>
 <?php
 $route = Route::current();
@@ -67,12 +72,12 @@ if($laman == 'pengesahan'){
             </ul>
         
             </nav>
-          <aside class="main-sidebar sidebar-light-danger elevation-4 bg-info text-white">
+          <aside class="main-sidebar sidebar-light-danger elevation-4 bg-custom text-white">
             <!-- Brand Logo -->
             <a href="{{url('/')}}" class="brand-link">
               <img src="/images/AdminLTELogo.png" alt="APM SMK" class="brand-image img-circle elevation-3"
                    style="opacity: .8">
-              <span class="brand-text font-weight-light">APM SMK</span>
+              <span class="brand-text font-weight-light text-white"><strong> APM SMK </strong></span>
             </a>
         
             <!-- Sidebar -->
