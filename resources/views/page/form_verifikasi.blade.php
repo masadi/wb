44be60f9f5d84aa5bed9f933aeb27b4797034fb4
masadi <masadi.com@gmail.com>
2020-11-04 00:30:41 +0700

    update

--- resources/views/page/form_verifikasi.blade.php
+++ resources/views/page/form_verifikasi.blade.php
@@ -73,7 +73,10 @@
                     <input type="hidden" name="instrumen_id[]" value="{{$item->instrumen_id}}">
                 </td>
                 <td rowspan="{{($item->telaah_dokumen_count + 1)}}">
-                    {{$item->pertanyaan}}</td>
+                    {{$item->pertanyaan}}
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </td>
             </tr>
             @foreach ($item->telaah_dokumen as $telaah_dokumen)
             <tr>
