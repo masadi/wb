ae810c63807c89482402822b340f68b182daf212
masadi <masadi.com@gmail.com>
2020-11-10 13:08:27 +0700

    Update validasi_instrumen.blade.php

--- resources/views/validasi_instrumen.blade.php
+++ resources/views/validasi_instrumen.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Verifikasi Isian Instrumen Sekolah')
+@section('title', 'Validasi Instrumen')
 @section('content')
 <div class="row">
     <div class="col-12">
